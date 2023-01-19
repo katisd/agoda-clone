4f74270c8014413b3f33418907e29c34176cb168
katisd <katidd17@gmail.com>
2023-01-19 19:44:37 +0700

    ✨ addd devViewPort

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-
+import DevViewport from "../common/components/DevViewport";
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      {process.env.NODE_ENV === "development" && <DevViewport />}
+    </>
+  );
 };
 
 export default MyApp;
